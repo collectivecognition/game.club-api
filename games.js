f65d15ebdadc9f3ee4e826080ef6daea2a47d282
William Blanchette <wb@wblanchette.com>
2017-01-04 19:38:32 -0700

    Typo

--- games.js
+++ games.js
@@ -16,14 +16,13 @@ router.get('/', function(req, res) {
   unirest.get("https://igdbcom-internet-game-database-v1.p.mashape.com/characters/?fields=*&limit=10")
     .header("X-Mashape-Key", process.env.MASHAPE_KEY)
     .header("Accept", "application/json")
-    .end(function (result) {
+    .end(function(result) {
       if(result.status >= 400){
         console.log(process.env.MASHAPE_KEY, result);
         return res.status(404).json({message:'Error'}); // FIXME
       }
 
       res.json(result.body);
-    });
 });
 
 module.exports = router;
