5703c506df0d0a8e68548473411d71b37ab010fa
William Blanchette <wb@wblanchette.com>
2017-01-04 19:58:43 -0700

    Changed param name

--- games.js
+++ games.js
@@ -10,7 +10,7 @@ var router = express.Router();
 // GET /games?q=:term
 
 router.get('/', function(req, res) {
-  unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?field=name,screenshots&limit=10&offset=0&order=release_dates.date%3Adesc&search=${req.query.q}`)
+  unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name,screenshots&limit=10&offset=0&order=release_dates.date%3Adesc&search=${req.query.q}`)
   .header('X-Mashape-Key', process.env.MASHAPE_KEY)
   .header('Accept', 'application/json')
   .end(function (result) {
