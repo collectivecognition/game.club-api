420ecd42e70a91900cd236f840594d918e573700
William Blanchette <wb@wblanchette.com>
2017-01-04 19:50:23 -0700

    Tweaks

--- games.js
+++ games.js
@@ -10,8 +10,8 @@ var router = express.Router();
 // GET /games?q=:term
 
 router.get('/', function(req, res) {
-unirest.get("https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10&offset=0&order=release_dates.date%3Adesc&search=zelda")
-.header("X-Mashape-Key", "BV34Hfrv90mshnUrEQkqRtxqbyQep1pWcMzjsnJkU4hn1igikD")
+unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=name&limit=10&offset=0&order=release_dates.date%3Adesc&search=${req.query.q}`)
+.header("X-Mashape-Key", process.env.MASHAPE_KEY)
 .header("Accept", "application/json")
 .end(function (result) {
   console.log(result.status, result.headers, result.body);
