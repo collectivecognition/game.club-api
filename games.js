e5196f8f70659746cc32d36029e42215ef1950d4
William Blanchette <wb@wblanchette.com>
2017-01-08 14:53:31 -0700

    Changed call

--- games.js
+++ games.js
@@ -32,46 +32,45 @@ router.get('/', (req, res) => {
 // GET /games/:id
 
 router.get('/:id', (req, res) => {
-  Game.findOne({igdbId: req.params.id})
-    .exec(function(err, game) {
-      console.log('err', err)
-      console.log('game', game)
-      // Game not found in db, retrieve it from igdb
+  Game.findOne({igdbId: req.params.id}, (err, game) =>
+    console.log('err', err)
+    console.log('game', game)
+    // Game not found in db, retrieve it from igdb
 
-      if(err){
-        return unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/games/${req.params.id}?fields=name,screenshots`)
-          .header('X-Mashape-Key', process.env.MASHAPE_KEY)
-          .header('Accept', 'application/json')
-          .end(result => {
-            
-            // Game found in igdb, insert it into db and return it
+    if(err){
+      return unirest.get(`https://igdbcom-internet-game-database-v1.p.mashape.com/games/${req.params.id}?fields=name,screenshots`)
+        .header('X-Mashape-Key', process.env.MASHAPE_KEY)
+        .header('Accept', 'application/json')
+        .end(result => {
+          
+          // Game found in igdb, insert it into db and return it
 
-              const game = new Game({
-                name: result.name,
-                igdbId: result.id,
-                imageUrl: 'TODO'
-              })
+            const game = new Game({
+              name: result.name,
+              igdbId: result.id,
+              imageUrl: 'TODO'
+            })
 
-              game.save((err, game) => {
-                if(err){
-                  return res.status(500).json({message: 'Problem inserting game in database.', error: err.message})
-                }
+            game.save((err, game) => {
+              if(err){
+                return res.status(500).json({message: 'Problem inserting game in database.', error: err.message})
+              }
 
-                res.json(game);
-              });
-          })
-          .catch(err => {
+              res.json(game);
+            });
+        })
+        .catch(err => {
 
-            // Game not found in igdb either, throw an error
+          // Game not found in igdb either, throw an error
 
-            res.status(404).json({message: 'Game not found.', error: err.message})
-          })
-      }
+          res.status(404).json({message: 'Game not found.', error: err.message})
+        })
+    }
 
-      // Game found in db, return it
+    // Game found in db, return it
 
-      res.json(game)
-    })
+    res.json(game)
+  })
 })
 
 module.exports = router
